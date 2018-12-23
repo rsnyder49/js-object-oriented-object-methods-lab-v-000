20a27b31fb8266a2faeef387461c8c28436af40c
Robert Snyder <r.snyder49@gmail.com>
2018-12-23 03:13:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember(name, homeState, training) {
   this.training = training;
   
   this.veto = function() {
-    console.log("No, I must disagree");
+    return "No, I must disagree";
   };
   
   this.approve = function() {
